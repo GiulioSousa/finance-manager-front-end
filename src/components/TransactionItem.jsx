c459f3588985a9ca70952b02264b281df49d5c75
Giulivan Sousa <giulivan_cs@hotmail.com>
2024-06-11 23:23:28 -0300

    Format: Formatação da data e preço/valor

--- src/components/TransactionItem.jsx
+++ src/components/TransactionItem.jsx
@@ -1,11 +1,23 @@
 const TransactionItem = ({ item }) => {
+
+    const formatDate = dateString => {
+        const [year, month, day] = dateString.split('-');
+        return `${day}/${month}/${year}`;
+    }
+
+    const formatPrice = price => {
+        const priceFloat = parseFloat(price).toFixed(2)
+        const priceString = priceFloat.toString().replace('.', ',')
+        return priceString
+    }
+
     return (
         <>
             <tr>
-                <td>{item.data_cad}</td>
+                <td>{formatDate(item.data_cad)}</td>
                 <td>{item.type}</td>
                 <td>{item.description}</td>
-                <td>R$ {item.price}</td>
+                <td>R$ {formatPrice(item.price)}</td>
                 <td>{item.category}</td>
                 <td>{item.status}</td>
                 <td>{item.account}</td>
